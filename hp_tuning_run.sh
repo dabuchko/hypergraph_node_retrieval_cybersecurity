ab961e13e989d9667835c3ee05aeb75088ce569b
Daniil Buchko <14076544@cuni.cz>
2026-03-21 22:30:01 +0100

    separating label propagation methods

--- hp_tuning_run.sh
+++ hp_tuning_run.sh
@@ -8,6 +8,7 @@ fi
 imbalance=${2:-none}
 
 # Feature based
+
 for f in "Logistic Regression" "MLP" "Naive Bayes" "KNN"; do
     python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Random Gaussian" --feature_based="$f"
     python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Matrix Factorization" --feature_based="$f"
@@ -18,7 +19,12 @@ for f in "Logistic Regression" "MLP" "Naive Bayes" "KNN"; do
 done
 
 # General graph based
-for f in "GCN" "GIN" "GAT" "GraphSAGE" "Label Propagation"; do
+
+for repr in "incidence" "clique"; do
+    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --graph_based="Label Propagation" --graph_repr_GNN="$repr"
+done
+
+for f in "GCN" "GIN" "GAT" "GraphSAGE"; do
     for repr in "incidence" "clique"; do
         python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Random Gaussian" --graph_based="$f" --graph_repr_GNN="$repr"
         python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Matrix Factorization" --graph_based="$f" --graph_repr_GNN="$repr"
@@ -30,7 +36,10 @@ for f in "GCN" "GIN" "GAT" "GraphSAGE" "Label Propagation"; do
 done
 
 # Hypergraph based
-for f in "HyperGCN" "UniGCN" "UniGAT" "UniGIN" "UniSAGE" "UniGCNII" "HCHA" "AllDeepSets" "AllSetTransformer" "HNHN" "HGNN" "CSP" "HyperSAGE" "MaxSum"; do
+
+python hyperparam_tuning.py "$1" --imbalance="$imbalance" --graph_based="CSP"
+
+for f in "HyperGCN" "UniGCN" "UniGAT" "UniGIN" "UniSAGE" "UniGCNII" "HCHA" "AllDeepSets" "AllSetTransformer" "HNHN" "HGNN" "HyperSAGE" "MaxSum"; do
     python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Random Gaussian" --graph_based="$f"
     python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Matrix Factorization" --graph_based="$f"
     python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Spectral Embedding" --graph_based="$f"
