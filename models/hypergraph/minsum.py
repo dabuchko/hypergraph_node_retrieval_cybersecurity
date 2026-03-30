dac3d23cbe351a81129059a4e7f0b712ab25cacd
Daniil Buchko <14076544@cuni.cz>
2026-03-30 21:46:25 +0200

    adding hyperedge weight support to MinSum

--- models/hypergraph/minsum.py
+++ models/hypergraph/minsum.py
@@ -4,7 +4,7 @@ from .unignn import HyperedgeAggregation
 
 class MinSum(MessagePassing):
     supports_hyperedge_attr = False
-    supports_hyperedge_weight = False
+    supports_hyperedge_weight = True
     def __init__(self, in_channels, hidden_channels, out_channels, num_layers, dropout=0.0):
         super().__init__("sum", aggr_kwargs={}, flow="source_to_target", node_dim=-2, decomposed_layers=1)
         self.hyperedge_prop = HyperedgeAggregation("min")
@@ -13,7 +13,7 @@ class MinSum(MessagePassing):
         self.dropout = torch.nn.Dropout(dropout, True)
         self.num_layers = num_layers
 
-    def forward(self, x, hyperedge_index):
+    def forward(self, x, hyperedge_index, hyperedge_weight = None):
         if len(x.shape)<2:
             x = x[:, None]
         # calculate the number of nodes and edges
@@ -21,6 +21,9 @@ class MinSum(MessagePassing):
         num_edges = 0
         if hyperedge_index.numel() > 0:
             num_edges = int(hyperedge_index[1].max()) + 1
+        if hyperedge_weight!=None:
+            hyperedge_weight = hyperedge_weight.reshape(-1, 1)
+            hyperedge_weight[hyperedge_weight==0] = 1e-9
 
         x = self.dropout(x)
         x = torch.relu(self.l1(x))
@@ -31,6 +34,8 @@ class MinSum(MessagePassing):
         for _ in range(self.num_layers):
             coef = 2 / (3 - coef)
             hyperedge_pass = self.hyperedge_prop(x, hyperedge_index, size=(num_nodes, num_edges))
+            if hyperedge_weight!=None:
+                hyperedge_pass = hyperedge_pass / hyperedge_weight
             x = (self.propagate(flipped_hyperedge_index, x=hyperedge_pass, size=(num_edges, num_nodes)) * (1-coef)) + (x*coef)
         
         x = self.dropout(x)
