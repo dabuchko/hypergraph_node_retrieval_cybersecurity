489f22623add998a6039e5819ccc72d334b2a814
Daniil Buchko <14076544@cuni.cz>
2026-04-03 13:33:10 +0200

    allow arbitrarily additional arguments for hyperparameter tuning script

--- hp_tuning_run.sh
+++ hp_tuning_run.sh
@@ -10,40 +10,40 @@ imbalance=${2:-none}
 # Feature based
 
 for f in "Logistic Regression" "MLP" "Naive Bayes" "KNN"; do
-    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Random Gaussian" --feature_based="$f"
-    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Matrix Factorization" --feature_based="$f"
-    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Spectral Embedding" --feature_based="$f"
-    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Spectral Embedding" --feature_based="$f" --graph_repr_embedding=clique
-    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Node2Vec" --feature_based="$f"
-    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Node2Vec" --feature_based="$f" --graph_repr_embedding=clique
+    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Random Gaussian" --feature_based="$f" "${@:3}"
+    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Matrix Factorization" --feature_based="$f" "${@:3}"
+    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Spectral Embedding" --feature_based="$f" "${@:3}"
+    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Spectral Embedding" --feature_based="$f" --graph_repr_embedding=clique "${@:3}"
+    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Node2Vec" --feature_based="$f" "${@:3}"
+    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Node2Vec" --feature_based="$f" --graph_repr_embedding=clique "${@:3}"
 done
 
 # General graph based
 
 for repr in "incidence" "clique"; do
-    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --graph_based="Label Propagation" --graph_repr_GNN="$repr"
+    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --graph_based="Label Propagation" --graph_repr_GNN="$repr" "${@:3}"
 done
 
 for f in "GCN" "GIN" "GAT" "GraphSAGE"; do
     for repr in "incidence" "clique"; do
-        python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Random Gaussian" --graph_based="$f" --graph_repr_GNN="$repr"
-        python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Matrix Factorization" --graph_based="$f" --graph_repr_GNN="$repr"
-        python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Spectral Embedding" --graph_based="$f" --graph_repr_GNN="$repr"
-        python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Spectral Embedding" --graph_based="$f" --graph_repr_GNN="$repr" --graph_repr_embedding=clique
-        python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Node2Vec" --graph_based="$f" --graph_repr_GNN="$repr"
-        python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Node2Vec" --graph_based="$f" --graph_repr_GNN="$repr" --graph_repr_embedding=clique
+        python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Random Gaussian" --graph_based="$f" --graph_repr_GNN="$repr" "${@:3}"
+        python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Matrix Factorization" --graph_based="$f" --graph_repr_GNN="$repr" "${@:3}"
+        python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Spectral Embedding" --graph_based="$f" --graph_repr_GNN="$repr" "${@:3}"
+        python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Spectral Embedding" --graph_based="$f" --graph_repr_GNN="$repr" --graph_repr_embedding=clique "${@:3}"
+        python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Node2Vec" --graph_based="$f" --graph_repr_GNN="$repr" "${@:3}"
+        python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Node2Vec" --graph_based="$f" --graph_repr_GNN="$repr" --graph_repr_embedding=clique "${@:3}"
     done
 done
 
 # Hypergraph based
 
-python hyperparam_tuning.py "$1" --imbalance="$imbalance" --graph_based="CSP"
+python hyperparam_tuning.py "$1" --imbalance="$imbalance" --graph_based="CSP" "${@:3}"
 
 for f in "HyperGCN" "UniGCN" "UniGAT" "UniGIN" "UniSAGE" "UniGCNII" "HCHA" "AllDeepSets" "AllSetTransformer" "HNHN" "HGNN" "HyperSAGE" "MinSum"; do
-    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Random Gaussian" --graph_based="$f"
-    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Matrix Factorization" --graph_based="$f"
-    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Spectral Embedding" --graph_based="$f"
-    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Spectral Embedding" --graph_based="$f" --graph_repr_embedding=clique
-    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Node2Vec" --graph_based="$f"
-    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Node2Vec" --graph_based="$f" --graph_repr_embedding=clique
+    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Random Gaussian" --graph_based="$f" "${@:3}"
+    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Matrix Factorization" --graph_based="$f" "${@:3}"
+    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Spectral Embedding" --graph_based="$f" "${@:3}"
+    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Spectral Embedding" --graph_based="$f" --graph_repr_embedding=clique "${@:3}"
+    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Node2Vec" --graph_based="$f" "${@:3}"
+    python hyperparam_tuning.py "$1" --imbalance="$imbalance" --embedding="Node2Vec" --graph_based="$f" --graph_repr_embedding=clique "${@:3}"
 done
